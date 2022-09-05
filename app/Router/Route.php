d0c390d52e810721e39dca4f03d99629a957c554
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-09-05 17:55:41 -0300

    handle route action controller and callback

--- app/Router/Route.php
+++ app/Router/Route.php
@@ -9,7 +9,20 @@ class Route
     public function __construct(public string $uri, array|callable $action, public string $method)
     {
         $this->uri = $uri;
-        $this->action = $action;
+        $this->action = $this->handleAction($action);
         $this->method = $method;
+    }
+
+    private function handleActionWithController(array $action): array
+    {
+        if (class_exists($action[0]))
+            $controller = new $action[0]();
+        return [$controller, $action[1]];
+    }
+
+    private function handleAction(array|callable $action): array|callable
+    {
+        if (is_array($action)) return $this->handleActionWithController($action);
+        return $action;
     }
 }
