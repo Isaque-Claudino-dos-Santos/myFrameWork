c6323a071348a60449d95af1f5f4e2567a5e65d3
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-09-10 00:39:22 -0300

    refactored to request route

--- app/Router/RouteRequest.php
+++ app/Router/RouteRequest.php
@@ -2,59 +2,57 @@
 
 namespace app\Router;
 
-use Exception;
-
 class RouteRequest
 {
     private string $requestUri;
     private string $requestMethod;
-    private Route $route;
-    private array $routeParamsValue;
 
-    public function __construct()
+    public function __construct(private Router $router)
     {
+        $this->router = $router;
         $this->requestUri = $_SERVER['REQUEST_URI'];
         $this->requestMethod = $_SERVER['REQUEST_METHOD'];
     }
 
     private function tranformUriParamsInRegex($uri)
     {
-        $pattern = '/{\w+}/';
-        $patternOpitional = '/' . URI_START_PARM . '\w+\?' . URI_END_PARM . '/';
-        return preg_replace([$pattern, $patternOpitional], ['(\w+)', '(\w+)?'], $uri);
+        $patternValueRequired = '/' . URI_START_PARM . '\w+' . URI_END_PARM . '/';
+        $patternValueOpitional = '/' . URI_START_PARM . '\w+\?' . URI_END_PARM . '/';
+        $replaceValueRequired = '(\w+)';
+        $replaceValueOptional = $patternValueRequired . '?';
+        return preg_replace(
+            [$patternValueRequired, $patternValueOpitional],
+            [$replaceValueRequired, $replaceValueOptional],
+            $uri
+        );
     }
 
-    private function setRouteParamsValue(array $ary): void
+    private function regexScapeBar(string $pattern)
     {
-        unset($ary[0][0]);
-        $params = $ary[0];
-        array_push($params, '');
-        $this->routeParamsValue = $params;
+        return str_replace('/', '\/', $pattern);
     }
 
-    private function setRouteRequired(Router $router)
+    private function handleUriRequired(array $routes)
     {
-        $routes = $router->getRoutes($this->requestMethod);
-        $routesKeys = array_keys($routes);
-        foreach ($routesKeys as $uri) {
-            $defaultUri = $uri;
-            $uri = $this->tranformUriParamsInRegex($defaultUri);
-            $pattern = str_replace('/', '\/', $uri);
-            $routeIsFound =  preg_match_all('/^' . $pattern . '$/', $this->requestUri, $match, PREG_SET_ORDER);
-            if ($routeIsFound) {
-                $this->route = $routes[$defaultUri];
-                $this->setRouteParamsValue($match);
-                return;
+        foreach ($routes as $uri => $route) {
+            $uriWithPattern = $this->tranformUriParamsInRegex($uri);
+            $uriWithPattern = $this->regexScapeBar($uriWithPattern);
+            $routeFound =  preg_match_all('/^' . $uriWithPattern . '$/', $this->requestUri, $match, PREG_SET_ORDER);
+            if ($routeFound) {
+                return [
+                    'uri' => $uri,
+                    'params' =>  array_slice($match[0], 1)
+                ];
             }
         }
-        throw new Exception('NOT FOUND', 404);
     }
 
-    public function run(Router $router)
+    public function run()
     {
-        $this->setRouteRequired($router);
-        $params = $this->routeParamsValue;
-        $action = $this->route->action;
-        call_user_func_array($action, $params);
+        $routes = $this->router->findRoutes($this->requestMethod);
+        $require = $this->handleUriRequired($routes->getRoutes()) ?? [];
+        $route = $routes->find($require['uri']);
+
+        call_user_func_array($route->action, $require['params']);
     }
 }
