7b4324f99d3dbb8809c013989f9670f0edda4896
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-09-10 13:56:32 -0300

    new arquiteture

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 require_once('../vendor/autoload.php');
 
 try {
-    require_once('../router/web.php');
+    require_once('../app/router/web.php');
 } catch (Exception $e) {
     echo $e->getMessage();
 }
