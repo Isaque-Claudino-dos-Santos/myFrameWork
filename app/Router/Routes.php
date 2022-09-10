39c710b1df2833b44d361f29dacf0576f49e6c54
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-09-10 00:40:26 -0300

    refactored with method and geters

--- app/Router/Routes.php
+++ app/Router/Routes.php
@@ -5,26 +5,54 @@ namespace app\Router;
 class Routes
 {
     private array $routes = [];
+    public readonly string $method;
+    private array $routeNames = [];
 
-    public function __construct(private readonly string $type)
+    public function __construct(string $method)
     {
-        $this->type = $type;
+        $this->method = $method;
     }
 
-    private function existeKeyInRoutes(string $uri): bool
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
+
+    private function setRouteNames(string $uri, string $name)
+    {
+        $this->routeNames[$uri] = $name;
+    }
+
+    private function uriExistInRoutes(string $uri): bool
     {
         return array_key_exists($uri, $this->routes);
     }
 
+    private function nameExistInRouteNames(string $name): bool
+    {
+        return array_search($name, $this->routeNames);
+    }
+
+    private function validateRoute(Route $route): void
+    {
+        if ($this->uriExistInRoutes($route->uri))
+            throw new \Exception('The uri( ' . $route->uri . ' ) passed in route ' . $this->method . ' already defined');
+
+        if ($this->nameExistInRouteNames($route->name))
+            throw new \Exception('The name( ' . $route->name . ' ) passed in route(' . $this->method . ' ' . $route->uri . ') already defined');
+    }
+
     public function add(Route $route): void
     {
+        $this->validateRoute($route);
+        $this->setRouteNames($route->uri, $route->name);
         $this->routes[$route->uri] = $route;
     }
 
     public function find(string $uri): Route
     {
-        if (!$this->existeKeyInRoutes($uri))
-            throw new \Exception('The uri(' . $uri . ') not exist found in routes');
+        if (!$this->uriExistInRoutes($uri))
+            throw new \Exception('The uri(' . $uri . ') not found in routes');
         return $this->routes[$uri];
     }
 }
