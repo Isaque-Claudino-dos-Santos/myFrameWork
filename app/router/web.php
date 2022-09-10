9ad45f9af867638965354ae4880b3dd788d50679
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-09-10 15:32:50 -0300

    reset

--- app/router/web.php
+++ app/router/web.php
@@ -12,25 +12,6 @@ use app\Controllers\UserController;
 
 $router = new Router();
 
-$router->get(
-    uri: '/',
-    action: [HomeController::class, 'index'],
-    name: 'home.index'
-);
-
-
-$router->get(
-    uri: '/user/{id}',
-    action: [UserController::class, 'show'],
-    name: 'user.show'
-);
-
-$router->post(
-    uri: '/user/{id}',
-    action: [UserController::class, 'store'],
-    name: 'user.store'
-);
-
 $routerRequest = new RouteRequest($router);
 
 $routerRequest->run();  
