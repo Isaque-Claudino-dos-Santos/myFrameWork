bd8c989832cb1da0e16418ee08dff39b04d99f71
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-09-10 22:04:21 -0300

    add route

--- app/router/web.php
+++ app/router/web.php
@@ -16,12 +16,13 @@ $router = new Router();
 $router->get('/', function () {
     echo "
     <a href='/user'>User Index</a> <br>
+    <a href='/user/test'>User Test</a> <br>
     ";
 }, 'home.index');
 
 $router->get('/user', [UserController::class, 'index'], 'user.index');
 
-
+$router->get('/user/{name?}/idade/{ages?}', [UserController::class, 'test'], 'user.test');
 
 
 $routerRequest = new RouteRequest($router);
