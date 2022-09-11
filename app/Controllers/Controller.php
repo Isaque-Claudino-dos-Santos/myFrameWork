d01ac4d5e4bfa2286e34fa6f2589056384c07868
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-09-11 18:43:48 -0300

    reset ./app/controller and /web.php

--- app/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace app\Controllers;
-
-use src\DataBase\DB;
-
-class Controller extends DB
-{
-}
--- app/router/web.php
+++ app/router/web.php
@@ -13,17 +13,6 @@ require_once('../src/helpers/dd.php');
 
 $router = new Router();
 
-$router->get('/', function () {
-    echo "
-    <a href='/user'>User Index</a> <br>
-    <a href='/user/test'>User Test</a> <br>
-    ";
-}, 'home.index');
-
-$router->get('/user', [UserController::class, 'index'], 'user.index');
-
-$router->get('/user/{name?}/idade/{ages?}', [UserController::class, 'test'], 'user.test');
-
 
 $routerRequest = new RouteRequest($router);
 $routerRequest->run();  
