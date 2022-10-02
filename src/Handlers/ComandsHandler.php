6bc13b3838d33f3bfa1f39cad419b5909b3bc92f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-10-02 15:32:13 -0300

    class with comands

--- src/Handlers/ComandsHandler.php
+++ src/Handlers/ComandsHandler.php
@@ -12,7 +12,7 @@ class ComandsHandler extends Kernel
         $this->migrationHandler = new MigrationsHandler();
     }
 
-    private function migration_up(string $migrationName = '')
+    private function comand_migration_up(string $migrationName = '')
     {
         if (empty($migrationName)) {
             foreach ($this->migrations as $migration) {
@@ -25,7 +25,7 @@ class ComandsHandler extends Kernel
         $this->migrationHandler->tableUp($migration);
     }
 
-    private function migration_down(string $migrationName = ''): void
+    private function comand_migration_down(string $migrationName = ''): void
     {
         if (empty($migrationName)) {
             foreach ($this->migrations as $migrateon) {
@@ -38,18 +38,26 @@ class ComandsHandler extends Kernel
         $this->migrationHandler->tableDown($migration);
     }
 
+    public function comand_serve(): void
+    {
+        exec('php -S localhost:2022 -t ./public');
+    }
+
     public function call(string $comand, string $argument): void
     {
-        if (empty($comand)) {
+        if (empty($comand) || !method_exists($this, 'comand_' . $comand)) {
             $comands = array_diff(get_class_methods($this), ['__construct', 'call']);
             echo "---------------------------\n";
             echo "----------COMANDS----------\n";
             echo "---------------------------\n";
             foreach ($comands as $comand) {
+                $comand = str_replace('comand_', '', $comand);
                 echo "\n---------------------------\n -> " . $comand . "\n" . "---------------------------\n";
             }
             return;
         };
+        $comand = 'comand_' . $comand;
+
         $this->$comand($argument);
     }
 }
